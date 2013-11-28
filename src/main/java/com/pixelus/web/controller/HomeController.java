811bacdac6e4066d765716a0096a328815cfbbcd
David Mouser <david.mouser@gmail.com>
2013-11-28 22:30:44 +1100

    Simple controller, view and related spring configuration.

---
+++ src/main/java/com/pixelus/web/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.pixelus.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String displayHome(Model model) {
+    return "/home";
+  }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+  <title>Home</title>
+</head>
+<body>
+<h2>Welcome!</h2>
+</body>
+</html>
