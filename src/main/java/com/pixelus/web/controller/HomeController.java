eea88309c098d08354f83ee328cf643ee6fc95b7
David Mouser <david.mouser@gmail.com>
2013-11-29 20:18:52 +1100

    Added support for application properties both statically defined directly in webapp.properties or declared dynamically as part of a maven 'profile'

--- src/main/java/com/pixelus/web/controller/HomeController.java
+++ src/main/java/com/pixelus/web/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.pixelus.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+  @Value("${static.property}")
+  private String staticProperty;
+
+  @Value("${dynamic.property}")
+  private String dynamicProperty;
+
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String displayHome(Model model) {
+
+    model.addAttribute("staticProperty", staticProperty);
+    model.addAttribute("dynamicProperty", dynamicProperty);
+
     return "/home";
   }
 }
--- src/test/java/com/pixelus/web/com.pixelus.web.controller/HomeControllerTest.java
+++ src/test/java/com/pixelus/web/com.pixelus.web.controller/HomeControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -35,6 +36,8 @@ public class HomeControllerTest {
     public void shouldGetHomeView() throws Exception {
         this.mockMvc.perform(get("/").accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
-                .andExpect(view().name("/home"));
+                .andExpect(view().name("/home"))
+                .andExpect(model().attribute("staticProperty", "test"))
+                .andExpect(model().attribute("dynamicProperty", "local"));
     }
 }
